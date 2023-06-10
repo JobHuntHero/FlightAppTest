fc581fea0f6e74c9fb611707bbe37eea6a7756c2
hakan demir <hakndemr49@gmail.com>
2023-06-10 12:58:15 -0700

    added new plugins

--- src/test/java/e2e/runner/RunnerClass.java
+++ src/test/java/e2e/runner/RunnerClass.java
@@ -1,20 +1,30 @@
 package e2e.runner;
 
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         features = "src/test/resources/features/",
-        glue = "e2e/stepDefinitions",
+
+        glue = "stepDefinitions",stepNotifications = true,
+
+        dryRun = false,
+
+        monochrome = true,
+
         tags = "@smoke",
-        dryRun = true,
-        plugin = {
-                "pretty", "html:target/cucumber.html","json:target/cucumber.json",
-                "rerun:target/failed.text"
+
+        plugin={"html:target/cucumber.html","pretty","json:target/cucumber.json",
+
+                "rerun:target/failed.txt"
+
+
         }
 )
+
 public class RunnerClass {
+
 }
